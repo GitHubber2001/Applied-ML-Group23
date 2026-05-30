454a14cc25a08daaae85f382541c29293a3a9fe3
GitHubber2001 <kevin-kuipers@home.nl>
2026-05-30 19:36:52 +0200

    created a temporary placeholder CNN model and some logic with training and evaluating but not fully functional and working yet

--- training/cnn_training.py
+++ training/cnn_training.py
@@ -1,34 +1,68 @@
-import joblib
-import numpy as np
-import pandas as pd
-import torch
-import torchvision.models as models
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
+import os
+import sys
 
-# use GPU
-current_accelerator = torch.accelerator.current_accelerator(True)
-if current_accelerator is not None:
-    device = current_accelerator.type
-else:
-    device = "cpu"
+file = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if file not in sys.path:
+    sys.path.insert(0, file)
 
-# datasets
-dev_df = pd.read_csv("dev_data.csv")
-image_df = pd.read_csv("image_data.csv")
-test_df = pd.read_csv("test_data.csv")
+from utilities.timer import TimeManager
 
-combined_df = pd.concat([image_df, dev_df, test_df], ignore_index=True)
-x = combined_df.drop(columns=["label"])
-y = combined_df["label"]
+with TimeManager("Imports"):
+    import joblib
+    import numpy as np
+    import pandas as pd
+    import torch
+    import torch.nn as nn
+    import torchvision.models as models
+    from sklearn.metrics import accuracy_score
+    from sklearn.model_selection import train_test_split
 
-x_train, x_test, y_train, y_test = train_test_split(
-    x, y, test_size=0.33, random_state=42, stratify=y
-)
+BATCH_SIZE = 64
 
-# model training saving (NOT FINISHED!)
 
-baseline_cnn = models.mobilenet_v3_small()  # placeholder
+def main():
+    # use GPU
+    current_accelerator = torch.accelerator.current_accelerator(True)
+    if current_accelerator is not None:
+        device = current_accelerator.type
+    else:
+        device = "cpu"
 
-baseline_cnn.train()
+    with TimeManager("Datasets"):
+        dev_df = pd.read_csv("Notebooks/dev_data.csv")
+        image_df = pd.read_csv("Notebooks/image_data.csv")
+        test_df = pd.read_csv("Notebooks/test_data.csv")
+
+        combined_df = pd.concat([image_df, dev_df, test_df], ignore_index=True)
+        x = combined_df.drop(columns=["label"])
+        y = combined_df["label"]
+
+        x_train, x_test, y_train, y_test = train_test_split(
+            x, y, test_size=0.33, random_state=42, stratify=y
+        )
+
+    with TimeManager("Training"):
+        # temporary placeholder model
+        cnn_model = models.mobilenet_v3_small()
+        cnn_model.train()
+
+    with TimeManager("Evaluating"):
+        cnn_model.eval()
+        with torch.no_grad():
+            pass
+
+        # temp
+        logits = None
+        probabilities = torch.nn.functional.softmax(logits)
+        predictions = (probabilities > 0.5).int()
+
+        print(f"\n\nAccuracy: {accuracy_score(y_test, predictions)}\n\n")
+
+    with TimeManager("Saving trained model"):
+        # joblib.dump(cnn_model, "trained_cnn.pkl")
+        pass
+
+
+if __name__ == "__main__":
+    with TimeManager("Program", False):
+        main()
