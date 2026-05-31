b1299af11a702cd5d4497c53f6cc8a8c6e5f1aea
GitHubber2001 <kevin-kuipers@home.nl>
2026-05-31 21:29:56 +0200

    put the csv data in a data folder but did not replace the original ones in notebooks folder because i dont want to break other code AND added code to see frequency/ratio of classes in test data

--- Notebooks/baseline_model_training.py
+++ Notebooks/baseline_model_training.py
@@ -1,13 +1,13 @@
 import joblib
-import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
-dev_df = pd.read_csv("dev_data.csv")
-image_df = pd.read_csv("image_data.csv")
-test_df = pd.read_csv("test_data.csv")
+dev_df = pd.read_csv("data/dev_data.csv")
+image_df = pd.read_csv("data/image_data.csv")
+test_df = pd.read_csv("data/test_data.csv")
+
 combined_df = pd.concat([image_df, dev_df, test_df], ignore_index=True)
 X = combined_df.drop(columns=["label"])
 y = combined_df["label"]
@@ -16,6 +16,13 @@ y = combined_df["label"]
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.33, random_state=42, stratify=y
 )
+
+test_freqency_classes = y_test.value_counts(normalize=False)
+test_ratio_classes = y_test.value_counts(normalize=True)
+
+print(test_freqency_classes)
+print(test_ratio_classes)
+
 random_forest = RandomForestClassifier(class_weight="balanced")
 
 random_forest.fit(X_train, y_train)
