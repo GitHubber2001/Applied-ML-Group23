dd12d4de0c0e58f75c51319c9b14d8b7fe8364aa
GitHubber2001 <kevin-kuipers@home.nl>
2026-06-01 14:50:46 +0200

    updated .gitignore and removed some unused files and folders

--- Notebooks/PCA.py
+++ Notebooks/PCA.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import cv2
@@ -11,9 +10,10 @@ from sklearn.metrics import accuracy_score
 import joblib
 
 
-train_dir = './chest_xray/chest_xray/train/'
+train_dir = "./chest_xray/chest_xray/train/"
 
 size = (256, 256)
+
 
 def extract_data(folder, label):
     data = []
@@ -26,8 +26,9 @@ def extract_data(folder, label):
             data.append(img.flatten())
     return np.array(data)
 
-penu_df = extract_data(train_dir, 'PNEUMONIA')
-normal_df = extract_data(train_dir, 'NORMAL')
+
+penu_df = extract_data(train_dir, "PNEUMONIA")
+normal_df = extract_data(train_dir, "NORMAL")
 data = np.vstack([penu_df, normal_df])
 
 dev_dir = "./chest_xray/val/"
@@ -51,7 +52,7 @@ new_data = ipca.inverse_transform(compressed)
 compressed_dev = ipca.transform(dev_data)
 compressed_test = ipca.transform(test_data)
 
-joblib.dump(ipca, 'pca_transform.pkl')
+joblib.dump(ipca, "pca_transform.pkl")
 
 image_df = pd.DataFrame(compressed)
 dev_df = pd.DataFrame(compressed_dev)
@@ -61,7 +62,7 @@ test_df = pd.DataFrame(compressed_test)
 n_penu = penu_df.shape[0]
 n_normal = normal_df.shape[0]
 labels = np.array([0] * n_penu + [1] * n_normal)
-image_df['label'] = labels
+image_df["label"] = labels
 
 n_penu = penu_df_dev.shape[0]
 n_normal = normal_df_dev.shape[0]
@@ -73,7 +74,7 @@ n_normal = normal_df_test.shape[0]
 labels = np.array([0] * n_penu + [1] * n_normal)
 test_df["label"] = labels
 
-image_df.to_csv('image_data.csv', index=False)
+image_df.to_csv("image_data.csv", index=False)
 dev_df.to_csv("dev_data.csv", index=False)
 test_df.to_csv("test_data.csv", index=False)
 
@@ -93,4 +94,4 @@ random_forest.fit(X_train, y_train)
 prediction = random_forest.predict(X_test)
 
 print(f"Accuracy: {accuracy_score(y_test, prediction)}")
-joblib.dump(random_forest, 'random_forest.pkl')
+joblib.dump(random_forest, "random_forest.pkl")
