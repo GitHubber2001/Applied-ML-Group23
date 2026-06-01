c0c0527d90c738d000b26cda12fe6118385f0a1c
GitHubber2001 <kevin-kuipers@home.nl>
2026-06-01 11:50:10 +0200

    improved random forest accuracy from 0.76 to 0.84

--- Notebooks/baseline_model_training.py
+++ Notebooks/baseline_model_training.py
@@ -2,7 +2,9 @@ import joblib
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV, train_test_split
+
+RANDOM_STATE = 42
 
 dev_df = pd.read_csv("data/dev_data.csv")
 image_df = pd.read_csv("data/image_data.csv")
@@ -14,22 +16,33 @@ y = combined_df["label"]
 
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.33, random_state=42, stratify=y
+    X, y, test_size=0.33, random_state=RANDOM_STATE, stratify=y
 )
 
+# for getting accuracy highest frequency guessing
 test_freqency_classes = y_test.value_counts(normalize=False)
 test_ratio_classes = y_test.value_counts(normalize=True)
-
 print(test_freqency_classes)
 print(test_ratio_classes)
 
-random_forest = RandomForestClassifier(class_weight="balanced")
+search_parameters = {
+    "n_estimators": list(range(24, 27, 1)),
+    "max_depth": [None, 30, 35, 40],
+}
 
-random_forest.fit(X_train, y_train)
+random_forest = RandomForestClassifier(random_state=RANDOM_STATE, n_estimators=25)
 
-prediction = random_forest.predict(X_test)
+search = GridSearchCV(random_forest, param_grid=search_parameters, cv=3, verbose=3)
+search.fit(X_train, y_train)
 
-print(f"Accuracy: {accuracy_score(y_test, prediction)}")
+best_random_forest = search.best_estimator_
+predictions = best_random_forest.predict(X_test)  # type: ignore
+
+sorted_results = pd.DataFrame(search.cv_results_).sort_values(by="rank_test_score")[
+    ["rank_test_score", "mean_test_score", "params"]
+]
+print(sorted_results)
+
+print(f"Accuracy random forest: {accuracy_score(y_test, predictions)}")
+
 joblib.dump(random_forest, "random_forest.pkl")
-
-# test comment for commit/push
