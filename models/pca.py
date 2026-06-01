91675f7b8bf877ef92b71887b20b7caa5285d534
mahmoodsaad123-spec <mahmoodsaad123@gmail.com>
2026-06-02 00:33:33 +0200

    Clean up pca.py by removing unnecessary comment
    Removed commented code for clarity.

--- models/pca.py
+++ models/pca.py
@@ -3,7 +3,7 @@ import os
 import joblib
 import numpy as np
 
-# Get path to pca_transform.pkl file in models directory
+
 _pca_path = os.path.join(os.path.dirname(__file__), "pca_transform.pkl")
 ipca = joblib.load(_pca_path)
 
